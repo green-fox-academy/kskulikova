7a6bbb5089bce9dc404f1f5a278c2a90f97add04
Ksenia Kulikova <kskulikovaa@gmail.com>
2019-04-04 15:24:38 +0200

    first five exercises for Loops

---
+++ week-01/day-4/src/PrintEven.java
@@ -0,0 +1,9 @@
+// Create a program that prints all the even numbers between 0 and 500
+public class PrintEven {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 501; i+=2) {
+            System.out.println(i);
+        }
+    }
+}
