e744aec1e3c56aa32855d30ee6e0028f30e8beb7
Ksenia Kulikova <kskulikovaa@gmail.com>
2019-04-08 12:02:47 +0200

    Create Sum

---
+++ week-02/day-1/src/Sum.java
@@ -0,0 +1,15 @@
+// Create the usual class wrapper and main method on your own.
+
+// Write a function called `sum` that returns the sum of numbers from zero to the given parameter
+public class Sum {
+    public static void main(String[] args) {
+        System.out.println(sum(10));
+    }
+    public static int sum(int i){
+        int sum = 0;
+        for (int j = 0; j < i + 1; j++){
+            sum += j;
+        }
+        return sum;
+    }
+}
