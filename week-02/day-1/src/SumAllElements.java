02acc2069928311c942ad8aa86bde6459c76e2e9
Ksenia Kulikova <kskulikovaa@gmail.com>
2019-04-08 15:03:03 +0200

    Create SumAllElements

---
+++ week-02/day-1/src/SumAllElements.java
@@ -0,0 +1,22 @@
+// - Create an array variable named `ai`
+//   with the following content: `[3, 4, 5, 6, 7]`
+// - Print the sum of the elements in `ai`
+
+public class SumAllElements {
+    public static void main(String[] args) {
+        int[] ai;
+        ai = new int[]{3, 4, 5, 6, 7};
+
+        System.out.println(sum(ai));
+    }
+
+    public static int sum(int[] arr) {
+        int sum = 0;
+        for (int i : arr) {
+            sum += i;
+        }
+        return sum;
+    }
+
+
+}
