c4126d233325dca9fe867c38261cf92207e47b1b
Ksenia Kulikova <kskulikovaa@gmail.com>
2019-04-08 14:02:39 +0200

    Create DoubleItems

---
+++ week-02/day-1/src/DoubleItems.java
@@ -0,0 +1,14 @@
+// - Create an array variable named `numList`
+//   with the following content: `[3, 4, 5, 6, 7]`
+// - Double all the values in the array
+
+public class DoubleItems {
+    public static void main(String[] args) {
+        int[] numList;
+        numList = new int[]{3, 4, 5, 6, 7};
+
+        for (int i = 0; i < numList.length; i++) {
+            numList[i] = numList[i] * 2;
+        }
+    }
+}
