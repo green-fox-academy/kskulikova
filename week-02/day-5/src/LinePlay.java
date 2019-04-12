5d92a485d713fef72cd14f8bb80c9debd947c3dd
Ksenia Kulikova <kskulikovaa@gmail.com>
2019-04-12 11:24:17 +0200

    feat(PlayLines): draw left & right corner lines w recursion

--- week-02/day-5/src/LinePlay.java
+++ week-02/day-5/src/LinePlay.java
@@ -10,21 +10,41 @@ public class LinePlay {
 
     public static void mainDraw(Graphics graphics) {
 
-        int startXLeft = 0;
-        int startYLeft = 80;
+        int startXLeft = 3;
+        int startYLeft = 81;
 
-        int endXLeft = 40;
-        int endYLeft = 560;
+        int endXLeft = 41;
+        int endYLeft = 557;
+
         graphics.setColor(Color.green);
         drawLinesLeft(startXLeft, startYLeft, endXLeft, endYLeft, graphics);
+
+        int startXRight = 557;
+        int startYRight = 479;
+
+        int endXRight = 517;
+        int endYRight = 3;
+
+        graphics.setColor(Color.magenta);
+        drawLinesRight(startXRight, startYRight, endXRight, endYRight, graphics);
+
     }
 
     private static void drawLinesLeft(int startX, int startY, int endX, int endY, Graphics graphics) {
 
-        if (endX < 560) {
+        if (endX < 520) {
 
             graphics.drawLine(startX, startY, endX, endY);
             drawLinesLeft(startX, startY + 40, endX + 40, endY, graphics);
+        }
+    }
+
+    private static void drawLinesRight(int startX, int startY, int endX, int endY, Graphics graphics) {
+
+        if (endX > 40) {
+
+            graphics.drawLine(startX, startY, endX, endY);
+            drawLinesRight(startX, startY - 40, endX - 40, endY, graphics);
         }
     }
 
