256fcd8d93af9f00decedc95871e63b5e49858d4
Ksenia Kulikova <kskulikovaa@gmail.com>
2019-04-16 11:41:42 +0200

    feat(WriteSingleLine) write nameto a file and catch IO exception

---
+++ week-03/day-2/src/WriteSingleLine.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.util.Collections;
+import java.util.List;
+
+// Write a function that is able to manipulate a file
+// By writing your name into it as a single line
+// The file should be named "my-file.txt"
+// In case the program is unable to write the file,
+// It should print the following error message: "Unable to write file: my-file.txt"
+
+public class WriteSingleLine {
+    public static void main(String[] args) {
+        String file = "my-file.txt";
+        String name = "Ksenia";
+        writeName(file, name);
+    }
+
+    private static void writeName(String file, String name) {
+        try {
+            Path src = Paths.get("assets/" + file);
+            Files.write(src, Collections.singleton(name));
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Unable to write file: my-file.txt");
+        }
+
+    }
+}
