61c821fec4599789423d68412a3b3a02dd279e13
Ksenia Kulikova <kskulikovaa@gmail.com>
2019-04-16 18:06:04 +0200

    feat(ReversedLines) reverse text keeping the original lines

---
+++ week-03/day-2/src/ReversedLines.java
@@ -0,0 +1,44 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReversedLines {
+
+    public static void main(String[] args) {
+        // Create a method that decrypts reversed-lines.txt
+        // Create a method that decrypts the duplicated-chars.txt
+        String filename = "reversed-lines.txt";
+        String copyTo = "decrypted.txt";
+        duplicatedChars(filename, copyTo);
+    }
+
+    private static void duplicatedChars(String copyFrom, String decrypted) {
+        List<String> list = new ArrayList<>();
+        String s = "";
+        String word = "";
+        try {
+            Path src = Paths.get("assets/" + copyFrom);
+            List<String> content = Files.readAllLines(src);
+            for (String line : content) {
+                String[] words = line.split(" ");
+                for (int i = words.length - 1; i >= 0; i--) {
+                    for (int j = words[i].length() - 1; j >= 0; j--) {
+                        s = s + words[i].charAt(j);
+                    }
+                    word = word + " " + s;
+                    s = "";
+                }
+                word += "\n";
+            }
+            list.add(word);
+
+            Files.write(Paths.get(decrypted), list);
+
+        } catch (IOException e) {
+            System.out.println("Could not open file: duplicated-chats.txt");
+        }
+    }
+}
