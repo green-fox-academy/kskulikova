5f19696804337b0e72ed35b9da3d5f22d9a6b17f
Ksenia Kulikova <kskulikovaa@gmail.com>
2019-04-16 11:07:33 +0200

    feat(DivideByZero) handle arithmetic exception with doubles

--- week-03/day-2/src/DivideByZero.java
+++ week-03/day-2/src/DivideByZero.java
@@ -5,13 +5,16 @@
 
 public class DivideByZero {
     public static void main(String[] args) {
-        divideBy(0);
+        divideBy(4.0);
     }
 
-    private static void divideBy(int i) {
-        int result;
+    private static void divideBy(double i) {
+        double result;
         try {
             result = 10 / i;
+            if (result == Double.POSITIVE_INFINITY ||
+                    result == Double.NEGATIVE_INFINITY)
+                throw new ArithmeticException();
             System.out.println(result);
         } catch (ArithmeticException e) {
             System.out.println("Division by zero!");
