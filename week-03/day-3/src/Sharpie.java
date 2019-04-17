1c7ae4825ec7986447a2074075c78a237c0f6997
Ksenia Kulikova <kskulikovaa@gmail.com>
2019-04-17 15:14:10 +0200

    feat(SharpieSet) create SharpieSet class with removeTrash and countUsable methods

--- week-03/day-3/src/Sharpie.java
+++ week-03/day-3/src/Sharpie.java
@@ -10,7 +10,7 @@ public class Sharpie {
 
     String color;
     float width;
-    static float inkAmount = 100;
+    float inkAmount = 100;
 
     public Sharpie(String color, float width) {
         this.color = color;
---
+++ week-03/day-3/src/SharpieSet.java
@@ -0,0 +1,68 @@
+//Reuse your Sharpie class
+//Create SharpieSet class
+//it contains a list of Sharpie
+//countUsable() -> sharpie is usable if it has ink in it
+//removeTrash() -> removes all unusable sharpies
+
+import java.util.ArrayList;
+
+public class SharpieSet {
+
+    ArrayList<Sharpie> sharpieSet = new ArrayList<>();
+
+    public void add(Sharpie s) {
+        sharpieSet.add(s);
+    }
+
+    public int countUsable() {
+        int count = 0;
+        for (Sharpie s : sharpieSet) {
+            if (isUsable(s)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public boolean isUsable(Sharpie s) {
+        if (s.inkAmount > 0) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    public void removeTrash() {
+        ArrayList<Sharpie> clean = new ArrayList<>();
+        for (Sharpie s : sharpieSet) {
+            if (isUsable(s)) {
+                clean.add(s);
+            }
+        }
+        sharpieSet = clean;
+    }
+
+
+    public static void main(String[] args) {
+        SharpieSet mySet = new SharpieSet();
+
+        mySet.add(new Sharpie("red", 1.5f));
+        mySet.add(new Sharpie("green", 2.9f));
+        mySet.add(new Sharpie("orange", 0.9f));
+
+
+        System.out.println(mySet.countUsable());
+
+        for (int i = 0; i < 100; i++) {
+            mySet.sharpieSet.get(2).use();
+        }
+        System.out.println(mySet.countUsable());
+
+        mySet.removeTrash();
+        System.out.println(mySet.sharpieSet.size());
+
+    }
+}
+
+
