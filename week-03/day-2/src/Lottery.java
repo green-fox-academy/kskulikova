7c3d2526339ed58ca8482e0040669c4f7befcaf8
Ksenia Kulikova <kskulikovaa@gmail.com>
2019-04-18 15:36:47 +0200

    feat(Lottery)read csv file and get unique lottery numbers

--- week-03/day-2/src/Lottery.java
+++ week-03/day-2/src/Lottery.java
@@ -1,4 +1,44 @@
-package PACKAGE_NAME;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
 
 public class Lottery {
+    public static void main(String[] args) {
+        String filename = "lottery.csv";
+        findMostCommonNumbers(5, filename);
+
+    }
+
+    public static void findMostCommonNumbers(int howMany, String filename) {
+
+        try {
+
+            Path src = Paths.get("assets/" + filename);
+            List<String> content = Files.readAllLines(src);
+            List<String > numbers = new ArrayList<>();
+
+            for (String line : content) {
+                String[] components = line.split(";");
+                for (String component : components) {
+                    if(component.length() <= 2){
+                        numbers.add(component);
+                    }
+                }
+            }
+
+            List<String> uniqueNumbers = new ArrayList<String>(new HashSet<String>(numbers));
+            System.out.println(uniqueNumbers);
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
 }
