052a8c6162238b51b8220fc75723f827201926da
Ksenia Kulikova <kskulikovaa@gmail.com>
2019-04-18 17:08:45 +0200

    feat(Lottery)sort Hashmap by value

--- week-03/day-2/src/Lottery.java
+++ week-03/day-2/src/Lottery.java
@@ -4,6 +4,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
 
+
 public class Lottery {
     public static void main(String[] args) {
         String filename = "lottery.csv";
@@ -17,28 +18,34 @@ public class Lottery {
 
             Path src = Paths.get("assets/" + filename);
             List<String> content = Files.readAllLines(src);
-            List<String > numbers = new ArrayList<>();
+            List<String> numbers = new ArrayList<>();
 
             for (String line : content) {
                 String[] components = line.split(";");
                 for (String component : components) {
-                    if(component.length() <= 2){
-                        numbers.add(component);
+                    if (!component.trim().equals("0") && !component.trim().equals("")) {
+                        if (component.trim().length() <= 2) {
+                            numbers.add(component.trim());
+                        }
                     }
                 }
             }
-            
-            HashMap numbersMap = new HashMap();
 
-            int counter = 0;
-             for (String number: numbers){
-                 if (numbersMap.containsKey(number)){
-                     numbersMap.put(number, counter++);
-                 } else {
-                     numbersMap.put(number, 1);
-                 }
-             }
-            System.out.println(numbersMap.size());
+            HashMap<String, Integer> numbersMap = new HashMap();
+
+            for (String number : numbers) {
+
+                if (numbersMap.containsKey(number)) {
+                    numbersMap.put(number, (numbersMap.get(number)) + 1);
+                } else {
+                    numbersMap.put(number, 1);
+                }
+            }
+
+            List<Map.Entry<String, Integer>> sorted = new ArrayList<>(numbersMap.entrySet());
+            sorted.sort(Map.Entry.comparingByValue());
+
+            System.out.println("map after sorting by values: " + sorted);
 
 
         } catch (IOException e) {
