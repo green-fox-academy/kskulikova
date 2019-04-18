23868a77b70fb2bff885d03573f36618218231fc
Ksenia Kulikova <kskulikovaa@gmail.com>
2019-04-18 10:36:06 +0200

    feat(Pirates)create armada class, war app

--- week-03/day-3/Pirates/src/Armada.java
+++ week-03/day-3/Pirates/src/Armada.java
@@ -1,2 +1,42 @@
+import java.lang.reflect.Array;
+import java.util.List;
+import java.util.Random;
+
 public class Armada {
+    int numberOfShips;
+    private Ship[] ships;
+
+    public Ship[] initializeArmada() {
+        numberOfShips = new Random().nextInt(50) + 15;
+        ships = new Ship[numberOfShips];
+        return ships;
+    }
+
+    public Ship[] fillArmada() {
+        for (int i = 0; i < numberOfShips; i++) {
+            ships[i] = new Ship();
+            ships[i].fillShip();
+        }
+        return ships;
+    }
+
+    public Ship[] getArmada() {
+        return ships;
+    }
+
+    public boolean war(Armada otherArmada) {
+        for (int i = 0; i < numberOfShips; i++) {
+            for (int j = 0; j < otherArmada.numberOfShips; j++) {
+                if (!getArmada()[i].battle(otherArmada.getArmada()[j])) {
+                    i++;
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+
+
 }
+
+
--- week-03/day-3/Pirates/src/WarApp.java
+++ week-03/day-3/Pirates/src/WarApp.java
@@ -1,2 +1,17 @@
 public class WarApp {
+    public static void main(String[] args) {
+
+
+        Armada armada1 = new Armada();
+        armada1.initializeArmada();
+        armada1.fillArmada();
+
+        Armada armada2 = new Armada();
+        armada2.initializeArmada();
+        armada2.fillArmada();
+
+        System.out.println("Did our armada win? " + armada1.war(armada2));
+        System.out.println("Did our armada win? " + armada1.war(armada2));
+
+    }
 }
