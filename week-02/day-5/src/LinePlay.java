0e9f47f95aa68ef595c5a909596c63b23d2744b1
Ksenia Kulikova <kskulikovaa@gmail.com>
2019-04-19 19:12:31 +0200

    feat(LinePlayQuarters)draw upper half

--- week-02/day-5/src/LinePlay.java
+++ week-02/day-5/src/LinePlay.java
@@ -1,73 +1,78 @@
-import javax.swing.*;
-
-import java.awt.*;
-
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 public class LinePlay {
-    static int WIDTH = 560;
-    static int HEIGHT = 560;
 
-    public static void mainDraw(Graphics graphics) {
-        int numberOfLines = 40;
-        int stepX = WIDTH / numberOfLines;
-        int stepY = HEIGHT / numberOfLines;
+  static int WIDTH = 310;
+  static int HEIGHT = 310;
 
-        int startXLeft = 2;
-        int startYLeft = stepY * 2 + 2;
+  public static void mainDraw(Graphics graphics) {
+    int numberOfLines = 14;
+    int stepX = WIDTH / numberOfLines;
+    int stepY = HEIGHT / numberOfLines;
 
-        int endXLeft = stepX + 2;
-        int endYLeft = HEIGHT - 2;
+    int startXLeft = 2;
+    int startYLeft = stepY * 2 + 2;
 
-        graphics.setColor(Color.green);
-        drawLinesLeft(startXLeft, startYLeft, endXLeft, endYLeft, graphics, stepX, stepY);
+    int endXLeft = stepX + 2;
+    int endYLeft = HEIGHT - 2;
 
-        int startXRight = WIDTH - 2;
-        int startYRight = HEIGHT - (stepY * 2 + 2);
+    graphics.setColor(Color.green);
+    drawLinesLeft(startXLeft, startYLeft, endXLeft, endYLeft, graphics, stepX, stepY);
 
-        int endXRight = WIDTH - (stepX + 2);
-        int endYRight = 2;
+    int startXRight = WIDTH - 2;
+    int startYRight = HEIGHT - (stepY * 2 + 2);
 
-        graphics.setColor(Color.magenta);
-        drawLinesRight(startXRight, startYRight, endXRight, endYRight, graphics, stepX, stepY);
+    int endXRight = WIDTH - (stepX + 2);
+    int endYRight = 2;
 
+    graphics.setColor(Color.magenta);
+    drawLinesRight(startXRight, startYRight, endXRight, endYRight, graphics, stepX, stepY);
+  }
 
+  private static void drawLinesLeft(int startX, int startY, int endX, int endY, Graphics graphics,
+      int stepX, int stepY) {
+
+    if (endX < WIDTH - stepX * 2) {
+
+      graphics.drawLine(startX, startY, endX, endY);
+      drawLinesLeft(startX, startY + stepY, endX + stepX, endY, graphics, stepX, stepY);
     }
+  }
 
-    private static void drawLinesLeft(int startX, int startY, int endX, int endY, Graphics graphics, int stepX, int stepY) {
+  private static void drawLinesRight(int startX, int startY, int endX, int endY, Graphics graphics,
+      int stepX, int stepY) {
 
-        if (endX < WIDTH - stepX * 2) {
+    if (endX > stepX) {
 
-            graphics.drawLine(startX, startY, endX, endY);
-            drawLinesLeft(startX, startY + stepY, endX + stepX, endY, graphics, stepX, stepY);
-        }
+      graphics.drawLine(startX, startY, endX, endY);
+      drawLinesRight(startX, startY - stepY, endX - stepX, endY, graphics, stepX, stepY);
     }
+  }
 
-    private static void drawLinesRight(int startX, int startY, int endX, int endY, Graphics graphics,int stepX, int stepY) {
 
-        if (endX > stepX) {
-
-            graphics.drawLine(startX, startY, endX, endY);
-            drawLinesRight(startX, startY - stepY, endX - stepX, endY, graphics,stepX, stepY);
-        }
-    }
-
-    public static void main(String[] args) {
-        JFrame jFrame = new JFrame("Drawing");
-        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        ImagePanel panel = new ImagePanel();
-        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
-        jFrame.add(panel);
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    ImagePanel panel = new ImagePanel();
+    panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+    jFrame.add(panel);
 //        jFrame.setLocationRelativeTo(null);
-        jFrame.setVisible(true);
-        jFrame.pack();
-    }
+    jFrame.setVisible(true);
+    jFrame.pack();
+  }
 
-    static class ImagePanel extends JPanel {
-        @Override
-        protected void paintComponent(Graphics graphics) {
-            super.paintComponent(graphics);
-            mainDraw(graphics);
-        }
+  static class ImagePanel extends JPanel {
+
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
     }
+  }
 }
---
+++ week-02/day-5/src/LinePlayQuarters.java
@@ -0,0 +1,84 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class LinePlayQuarters {
+
+  static int WIDTH = 560;
+  static int HEIGHT = 560;
+
+//    static int width = WIDTH;
+//    static int height = HEIGHT;
+
+  public static void mainDraw(Graphics graphics, int width, int height, int n) {
+    width = WIDTH / n;
+    height = HEIGHT / n;
+    int numberOfLines = 25;
+    int stepX = width / numberOfLines;
+    int stepY = height / numberOfLines;
+
+    for (int i = 0; i < n; i++) {
+      int startXLeft = 3 + width * i;
+      int startYLeft = stepY * 2 + 3;
+
+      int endXLeft = startXLeft * i + stepX + 3;
+      int endYLeft = height - 3;
+
+      graphics.setColor(Color.green);
+      drawLinesLeft(startXLeft, startYLeft, endXLeft, endYLeft, graphics, stepX, stepY, width, i);
+
+      int startXRight = width - 3 + width * i;
+      int startYRight = height - (stepY * 2 + 3);
+
+      int endXRight = width - (stepX + 3) + width * i;
+      int endYRight = 3;
+
+      graphics.setColor(Color.magenta);
+      drawLinesRight(startXRight, startYRight, endXRight, endYRight, graphics, stepX, stepY, width,
+          i);
+    }
+  }
+
+  private static void drawLinesLeft(int startX, int startY, int endX, int endY, Graphics graphics,
+      int stepX, int stepY, int width, int i) {
+
+    if (endX < width - stepX * 2 + width * i) {
+
+      graphics.drawLine(startX, startY, endX, endY);
+      drawLinesLeft(startX, startY + stepY, endX + stepX, endY, graphics, stepX, stepY, width, i);
+    }
+  }
+
+  private static void drawLinesRight(int startX, int startY, int endX, int endY, Graphics graphics,
+      int stepX, int stepY, int width, int i) {
+
+    if (endX > width * i + stepX) {
+
+      graphics.drawLine(startX, startY, endX, endY);
+      drawLinesRight(startX, startY - stepY, endX - stepX, endY, graphics, stepX, stepY, width, i);
+    }
+  }
+
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    ImagePanel panel = new ImagePanel();
+    panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+    jFrame.add(panel);
+//        jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+    jFrame.pack();
+  }
+
+  static class ImagePanel extends JPanel {
+
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      int n = 2;
+      mainDraw(graphics, WIDTH, HEIGHT, n);
+    }
+  }
+}
