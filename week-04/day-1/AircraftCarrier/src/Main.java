dc06c76803bfec569469bf221505598612c01daf
Ksenia Kulikova <kskulikovaa@gmail.com>
2019-04-24 20:15:18 +0200

    add(AircraftCarrier) add main class for testing

---
+++ week-04/day-1/AircraftCarrier/src/Main.java
@@ -0,0 +1,36 @@
+public class Main {
+
+  public static void main(String[] args) throws Throwable {
+    Carrier myCarrier = new Carrier(500, 5);
+    Carrier enemyCarrier = new Carrier(60, 3);
+
+    F35 first = new F35();
+    F35 second = new F35();
+    F16 third = new F16();
+
+    first.refill(100);
+    second.refill(4);
+    third.refill(5);
+
+    myCarrier.add(first);
+    myCarrier.add(second);
+
+    first.fight();
+
+    for (int i = 0; i < 5; i++) {
+      enemyCarrier.add(new F35());
+    }
+    enemyCarrier.add(third);
+
+    System.out.println(enemyCarrier.getStatus());
+
+    enemyCarrier.fill();
+
+    System.out.println(enemyCarrier.getStatus());
+
+    myCarrier.fight(enemyCarrier);
+
+    System.out.println(myCarrier.getStatus());
+    System.out.println(enemyCarrier.getStatus());
+  }
+}
