c6aca3ec151ce5c0f6457aca8160f8c30e79eb1e
Ksenia Kulikova <kskulikovaa@gmail.com>
2019-04-24 11:06:41 +0200

    feat(Student) override clone method

---
+++ week-04/day-1/gardenApp/src/Plant.java
@@ -0,0 +1,41 @@
+public class Plant {
+
+  private int waterAmount;
+  private boolean needsWater;
+
+  private String getColor() {
+    return color;
+  }
+
+  private String color;
+
+
+  int getWaterAmount() {
+    return waterAmount;
+  }
+
+  void setWaterAmount(int waterAmount) {
+    this.waterAmount = waterAmount;
+  }
+
+  public boolean isNeedsWater() {
+    return needsWater;
+  }
+
+
+  public void getSomeWater(int waterAmount) {
+    System.out.println("Watering with " + waterAmount + " water");
+  }
+
+  @Override
+  public String toString() {
+    return ("The " + this.getColor() + " " + this.getClass().getName() + (isNeedsWater() ? " needs"
+        : " doesn't need") + " water \n");
+  }
+
+  Plant(String color) {
+    needsWater = true;
+    waterAmount = 0;
+    this.color = color;
+  }
+}
---
+++ week-04/day-2/src/Student.java
@@ -0,0 +1,42 @@
+public class Student extends Person {
+
+  private String previousOrganization;
+  private int skippedDays;
+
+  Student() {
+    super();
+    previousOrganization = "The School of Life";
+    skippedDays = 0;
+  }
+
+  Student(String name, int age, String gender, String previousOrganization) {
+    super(name, age, gender);
+    this.previousOrganization = previousOrganization;
+    this.skippedDays = 0;
+
+  }
+
+  @Override
+  void introduce() {
+    System.out.println(
+        "Hi, I'm " + this.getName() + ", a " + this.getAge() + " year old " + this.getGender()
+            + " from " + previousOrganization +
+            " who skipped " + skippedDays + " days from the course already.");
+  }
+
+  @Override
+  void getGoal() {
+    System.out.println("My goal is: Be a junior software developer.");
+  }
+
+  void skipDays(int numberOfDays) {
+    this.skippedDays += numberOfDays;
+  }
+
+
+  public Student clone() {
+    Student student = this;
+    return student;
+  }
+
+}
