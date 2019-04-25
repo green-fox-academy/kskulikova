64aeac5cb8588838c7199f85637cfc08d8f57257
Ksenia Kulikova <kskulikovaa@gmail.com>
2019-04-25 15:44:47 +0200

    test(Fibonacci)get n-th Fibonacci number

--- week-04/day-3/Fibonacci/src/Fibonacci.java
+++ week-04/day-3/Fibonacci/src/Fibonacci.java
@@ -1,12 +1,7 @@
-import java.util.ArrayList;
-import java.util.List;
 
 class Fibonacci {
 
-  int result;
-
   int fibonacci(int n) {
-    List<Integer> fibonacci = new ArrayList<>();
 
     if (n == 1 || n == 0) {
       return n;
