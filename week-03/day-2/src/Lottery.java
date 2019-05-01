e846a3455a4cbd45ac4370bf26cf02d5e8c2df04
Ksenia Kulikova <kskulikovaa@gmail.com>
2019-05-01 13:51:44 +0200

    fix(Lottery): take only lottery numbers into account

--- week-03/day-2/src/Lottery.java
+++ week-03/day-2/src/Lottery.java
@@ -6,62 +6,62 @@ import java.util.*;
 
 
 public class Lottery {
-    public static void main(String[] args) {
-        String filename = "lottery.csv";
-        System.out.println("5 most common lottery numbers: " + findMostCommonNumbers(5, filename));
 
-    }
+  public static void main(String[] args) {
+    String filename = "lottery.csv";
+    System.out.println("5 most common lottery numbers: " + findMostCommonNumbers(5, filename));
 
-    public static String findMostCommonNumbers(int howMany, String filename) {
-        String[] list = new String[howMany];
+  }
 
-        try {
+  public static String findMostCommonNumbers(int howMany, String filename) {
+    String[] list = new String[howMany];
 
-            Path src = Paths.get("assets/" + filename);
-            List<String> content = Files.readAllLines(src);
-            List<String> numbers = new ArrayList<>();
+    try {
 
-            for (String line : content) {
-                String[] components = line.split(";");
-                for (String component : components) {
-                    if (!component.trim().equals("0") && !component.trim().equals("")) {
-                        if (component.trim().length() <= 2) {
-                            numbers.add(component.trim());
-                        }
-                    }
-                }
-            }
+      Path src = Paths.get("assets/" + filename);
+      List<String> content = Files.readAllLines(src);
+      List<String> numbers = new ArrayList<>();
 
-            HashMap<String, Integer> numbersMap = new HashMap<>();
-
-            for (String number : numbers) {
-
-                if (numbersMap.containsKey(number)) {
-                    numbersMap.put(number, (numbersMap.get(number)) + 1);
-                } else {
-                    numbersMap.put(number, 1);
-                }
-            }
-
-            List<Map.Entry<String, Integer>> sorted = new ArrayList<>(numbersMap.entrySet());
-            sorted.sort(Map.Entry.comparingByValue());
-
-            int i = 0;
-            for (int mapIndex = sorted.size() - 1; mapIndex >= sorted.size() - howMany; mapIndex--) {
-                list[i] = sorted.get(mapIndex).getKey();
-                i++;
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
+      for (String line : content) {
+        String[] components = line.split(";");
+        for (int i = 11; i < 16; i++) {
+          numbers.add(components[i]);
         }
 
-        String finalList = "";
-        for (int i = 0; i < list.length; i++) {
-            finalList += list[i] + " ";
-        }
-        return finalList;
+      }
 
+      HashMap<String, Integer> numbersMap = new HashMap<>();
+
+      for (String number : numbers) {
+
+        if (numbersMap.containsKey(number)) {
+          numbersMap.put(number, (numbersMap.get(number)) + 1);
+        } else {
+          numbersMap.put(number, 1);
+        }
+      }
+
+      System.out.println(numbersMap.get("1"));
+
+      List<Map.Entry<String, Integer>> sorted = new ArrayList<>(numbersMap.entrySet());
+      sorted.sort(Map.Entry.comparingByValue());
+
+      int i = 0;
+      for (int mapIndex = sorted.size() - 1; mapIndex >= sorted.size() - howMany; mapIndex--) {
+        list[i] = sorted.get(mapIndex).getKey();
+        i++;
+      }
+
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+
+    String finalList = "";
+    for (int i = 0; i < list.length; i++) {
+      finalList += list[i] + " ";
+    }
+    return finalList;
+
+  }
 
 }
