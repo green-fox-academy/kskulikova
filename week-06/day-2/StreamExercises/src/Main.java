1ace3b07efce5eb7e8dbb5727ea591011d635fc8
Ksenia Kulikova <kskulikovaa@gmail.com>
2019-05-14 11:35:41 +0200

    feat(StreamExercises): get squares of positive numbrers in list

--- week-06/day-2/StreamExercises/src/Main.java
+++ week-06/day-2/StreamExercises/src/Main.java
@@ -1,16 +1,27 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Main {
 
   public static void main(String[] args) {
     List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
+    //Exercise1
     //without a variable
     numbers.stream().filter(n -> n % 2 == 0).forEach(System.out::println);
     //storing in a variable
     List<Integer> evenNumbers = numbers.stream().filter(n -> n % 2 == 0)
         .collect(Collectors.toList());
     evenNumbers.forEach(System.out::println);
+
+    //Exercise2
+    //without a variable
+    numbers.stream().filter(n -> n > 0).map(n -> n*n).forEach(System.out::println);
+    //with a variable
+    List<Integer> positiveSquares = numbers.stream().filter(n -> n > 0).map(n->n*n).collect(
+        Collectors.toList());
+    positiveSquares.forEach(System.out::println);
+
   }
 }
