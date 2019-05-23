26219cba428821783d5635abe5b00b2903a75057
Ksenia Kulikova <kskulikovaa@gmail.com>
2019-05-23 14:31:02 +0200

    feat(ProgrammersFoxClub): return a message if given name is in the list or not

--- week-07/day-4/programmerfoxclub/src/main/java/com/greenfoxacademy/programmerfoxclub/controllers/MainController.java
+++ week-07/day-4/programmerfoxclub/src/main/java/com/greenfoxacademy/programmerfoxclub/controllers/MainController.java
@@ -1,5 +1,7 @@
 package com.greenfoxacademy.programmerfoxclub.controllers;
 
+import com.greenfoxacademy.programmerfoxclub.models.Fox;
+import com.greenfoxacademy.programmerfoxclub.services.FoxService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,9 +14,17 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @RequestMapping("/")
 public class MainController {
 
+  private FoxService foxservice;
+
+  MainController(FoxService foxservice) {
+    this.foxservice = foxservice;
+  }
+
   @RequestMapping()
-  public String staticFox(@RequestParam String name, Model model) {
-    model.addAttribute("name", name);
+  public String staticFox(@RequestParam(value = "name", required = false) String name, Model model,
+      Fox fox) {
+    foxservice.getFoxList();
+    model.addAttribute("fox", fox);
     return "index";
   }
 
@@ -24,9 +34,26 @@ public class MainController {
   }
 
   @PostMapping("login")
-  public String login(@RequestParam String name, RedirectAttributes redirectAttributes) {
-    redirectAttributes.addAttribute("name", name);
-    return "redirect:/";
+  public String login(@RequestParam String name, RedirectAttributes redirectAttributes,
+      Model model) {
+    model.addAttribute("foxExists", foxservice.checkFox(name));
+    model.addAttribute("message", foxservice.getMessage(name));
+    redirectAttributes.addAttribute("fox", foxservice.getFox(name));
+    if (foxservice.checkFox(name)) {
+      return "redirect:/?name=" + name;
+    } else {
+      return "login";
+    }
   }
 
+  @GetMapping("add")
+  public String add() {
+    return "add";
+  }
+
+  @PostMapping("add")
+  public String addFox(@RequestParam String name) {
+    foxservice.add(name);
+    return "redirect:/?name=" + name;
+  }
 }
---
+++ week-07/day-4/programmerfoxclub/src/main/java/com/greenfoxacademy/programmerfoxclub/services/FoxService.java
@@ -0,0 +1,47 @@
+package com.greenfoxacademy.programmerfoxclub.services;
+
+import com.greenfoxacademy.programmerfoxclub.models.Fox;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FoxService {
+
+  private ArrayList<Fox> foxList;
+
+  FoxService() {
+    foxList = new ArrayList<>();
+    foxList.add(new Fox("Fry"));
+    foxList.add(new Fox("Leela"));
+    foxList.add(new Fox("Bender"));
+  }
+
+  public List<Fox> getFoxList() {
+    return foxList;
+  }
+
+  public Fox getFox(String name) {
+    return foxList.stream().filter(f -> f.getName().equals(name)).findAny().orElse(null);
+  }
+
+  public boolean checkFox(String name) {
+
+    return foxList.stream().anyMatch(f -> f.getName().equals(name));
+  }
+
+  public String getMessage(String name) {
+    String message = "";
+    if (!checkFox(name)) {
+      message = "You have provided a name that has not been used before, add it as a new one!";
+    }
+    return message;
+  }
+
+  public void add(String name) {
+
+    foxList.add(new Fox(name));
+
+  }
+
+}
