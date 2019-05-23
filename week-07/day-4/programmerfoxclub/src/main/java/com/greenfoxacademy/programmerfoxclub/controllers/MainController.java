5c9edb31b10e5499714a4117b343617f1d1c0e81
Ksenia Kulikova <kskulikovaa@gmail.com>
2019-05-23 10:24:04 +0200

    feat(ProgrammersFoxClub): redirect with name parameter from login page to main

--- week-07/day-4/programmerfoxclub/src/main/java/com/greenfoxacademy/programmerfoxclub/controllers/MainController.java
+++ week-07/day-4/programmerfoxclub/src/main/java/com/greenfoxacademy/programmerfoxclub/controllers/MainController.java
@@ -1,20 +1,32 @@
 package com.greenfoxacademy.programmerfoxclub.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/")
 public class MainController {
 
   @RequestMapping()
-  public String staticFox() {
+  public String staticFox(@RequestParam String name, Model model) {
+    model.addAttribute("name", name);
     return "index";
   }
 
-  @RequestMapping("login")
-  public String login() {
+  @GetMapping("login")
+  public String startLogin() {
     return "login";
+  }
+
+  @PostMapping("login")
+  public String login(@RequestParam String name, RedirectAttributes redirectAttributes) {
+    redirectAttributes.addAttribute("name", name);
+    return "redirect:/";
   }
 
 }
