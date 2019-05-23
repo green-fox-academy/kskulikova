422a857f0d2b93dc1e824d6002011e9f6bc2214c
Ksenia Kulikova <kskulikovaa@gmail.com>
2019-05-23 09:55:29 +0200

    add(ProgrammersFoxClub): add login page

--- week-07/day-4/programmerfoxclub/src/main/java/com/greenfoxacademy/programmerfoxclub/controllers/MainController.java
+++ week-07/day-4/programmerfoxclub/src/main/java/com/greenfoxacademy/programmerfoxclub/controllers/MainController.java
@@ -4,10 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/")
 public class MainController {
 
-  @RequestMapping("/")
+  @RequestMapping()
   public String staticFox() {
     return "index";
   }
+
+  @RequestMapping("login")
+  public String login() {
+    return "login";
+  }
+
 }
