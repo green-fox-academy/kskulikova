3f4fe5d6d1bca01a9b9e04bb5e701d261b964372
Ksenia Kulikova <kskulikovaa@gmail.com>
2019-06-16 22:24:51 +0200

    add(libraryexample): add missing files

---
+++ week-07/day-1/libraryexample/src/main/java/com/greenfoxacademy/libraryexample/LibraryexampleApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.libraryexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryexampleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LibraryexampleApplication.class, args);
+  }
+}
